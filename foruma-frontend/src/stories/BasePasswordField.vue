f0ab26b397350555384d1dc3dc67d527f17fd935
agabani <agabani@users.noreply.github.com>
2021-05-08 18:59:08 +0100

    feat(frontend): add base password field

---
+++ foruma-frontend/src/stories/BasePasswordField.vue
@@ -0,0 +1,82 @@
+<template>
+  <input
+    :class="classes"
+    type="password"
+    :placeholder="placeholder"
+    v-model="value"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "BasePasswordField",
+  props: {
+    initialValue: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    size: {
+      type: String,
+      default: "medium",
+      validator: function (value: string): boolean {
+        return ["small", "medium", "large"].indexOf(value) !== -1;
+      },
+    },
+  },
+  emits: ["change"],
+  data() {
+    return {
+      value: this.initialValue,
+    };
+  },
+  computed: {
+    classes(): string {
+      return ["base-password-field", `base-password-field--${this.size}`].join(
+        " "
+      );
+    },
+  },
+  watch: {
+    value(newValue, oldValue) {
+      this.$emit("change", { newValue, oldValue });
+    },
+  },
+});
+</script>
+
+<style lang="css" scoped>
+.base-password-field {
+  border: 0;
+  border-radius: 3em;
+  cursor: text;
+}
+
+.base-password-field:focus {
+  outline: none;
+}
+
+.base-password-field--small {
+  font-size: 12px;
+  padding: 10px 16px;
+}
+
+.base-password-field--medium {
+  font-size: 14px;
+  padding: 11px 20px;
+}
+
+.base-password-field--large {
+  font-size: 16px;
+  padding: 12px 24px;
+}
+
+.base-password-field {
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.15) 0 0 0 1px inset;
+}
+</style>
