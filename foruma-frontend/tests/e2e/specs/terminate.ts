b5839f33aac70490b454f2796667b4cd63c00aed
agabani <agabani@users.noreply.github.com>
2021-05-25 00:12:39 +0100

    feat: terminate e2e tests

--- foruma-frontend/tests/e2e/specs/terminate.ts
+++ foruma-frontend/tests/e2e/specs/terminate.ts
@@ -1,13 +1,12 @@
-describe("authentication", () => {
+describe("terminate", () => {
   it("should sign up, log out, log in, log out, log in, terminate account", () => {
     const username = Cypress._.uniqueId(Date.now().toString());
     const password = Cypress._.uniqueId(Date.now().toString());
 
     cy.visit("/");
 
-    // sign up
+    // signup
     cy.get(".base-header").contains("Sign up").click();
-
     cy.url().should("include", "/signup");
 
     cy.get('.pure-signup-form input[type="text"]')
@@ -15,7 +14,7 @@ describe("authentication", () => {
       .should("have.value", username);
 
     cy.get('.pure-signup-form input[type="password"]').each((element) => {
-      cy.wrap(element).type(password).should("have.value", password);
+      cy.wrap(element).clear().type(password).should("have.value", password);
     });
 
     cy.get('.pure-signup-form button[type="submit"]').click();
@@ -24,54 +23,47 @@ describe("authentication", () => {
 
     // log out
     cy.get(".base-header").contains("Log out").click();
-
     cy.get(".base-header").contains("Sign up");
 
     // log in
     cy.get(".base-header").contains("Log in").click();
-
     cy.url().should("include", "/login");
 
-    cy.get('.base-login-form input[type="text"]')
+    cy.get('.pure-login-form input[type="text"]')
       .type(username)
       .should("have.value", username);
 
-    cy.get('.base-login-form input[type="password"]')
+    cy.get('.pure-login-form input[type="password"]')
       .type(password)
       .should("have.value", password);
 
-    cy.get('.base-login-form button[type="submit"]').click();
-
-    cy.get(".base-header").contains(username);
-
-    // log lout
-    cy.get(".base-header").contains("Log out").click();
-
-    cy.get(".base-header").contains("Log in");
-
-    // log in
-    cy.reload(); // reset the form
-
-    cy.get(".base-header").contains("Log in").click();
-
-    cy.url().should("include", "/login");
-
-    cy.get('.base-login-form input[type="text"]')
-      .type(username)
-      .should("have.value", username);
-
-    cy.get('.base-login-form input[type="password"]')
-      .type(password)
-      .should("have.value", password);
-
-    cy.get('.base-login-form button[type="submit"]').click();
-
-    cy.get(".base-header").contains(username);
+    cy.get('.pure-login-form button[type="submit"]').click();
 
     // terminate account
     cy.get(".base-header").contains(username).click();
 
     cy.contains("Delete account").click();
+
+    cy.get(".base-header").contains("Log in");
+
+    // login
+    cy.get(".base-header").contains("Log in").click();
+    cy.url().should("include", "/login");
+
+    cy.get('.pure-login-form input[type="text"]')
+      .type(username)
+      .should("have.value", username);
+
+    cy.get('.pure-login-form input[type="password"]')
+      .type(password)
+      .should("have.value", password);
+
+    cy.get('.pure-login-form button[type="submit"]').click();
+
+    cy.contains("Sorry! There was a problem with your request!");
+    cy.contains("Sorry! There was a problem with your request!");
+
+    cy.get(".base-alert--close").click();
 
     cy.get(".base-header").contains("Log in");
   });
