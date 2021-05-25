a0b3511bcb60c55085ac566a430912fa59082ed3
agabani <agabani@users.noreply.github.com>
2021-05-25 23:09:56 +0100

    feat: pure change password panel

---
+++ foruma-frontend/src/stories/PureChangePasswordPanel.stories.js
@@ -0,0 +1,42 @@
+import PureChangePasswordPanel from "./PureChangePasswordPanel.vue";
+
+export default {
+  title: "Components/PureChangePasswordPanel",
+  component: PureChangePasswordPanel,
+  argTypes: {
+    alertType: {
+      control: {
+        type: "select",
+        options: ["information", "success", "warning"],
+      },
+    },
+    onSubmit: {},
+  },
+};
+
+const Template = (args) => ({
+  components: { PureChangePasswordPanel },
+  setup() {
+    return { args };
+  },
+  template: `<PureChangePasswordPanel v-bind="args" />`,
+});
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const SuccessAlert = Template.bind({});
+SuccessAlert.args = {
+  ...Default.args,
+  alertEventDate: new Date(),
+  alertTitle: "Your password has been changed",
+  alertType: "success",
+};
+
+export const WarningAlert = Template.bind({});
+WarningAlert.args = {
+  ...Default.args,
+  alertEventDate: new Date(),
+  alertTitle: "Uh oh, something went wrong",
+  alertMessage: "Make sure your passwords match then try again!",
+};
