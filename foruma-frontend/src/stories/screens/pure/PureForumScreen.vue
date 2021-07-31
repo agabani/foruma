be6630236be318da597791621d79ef24da42de33
agabani <agabani@users.noreply.github.com>
2021-07-31 21:40:07 +0100

    feat: pure forum screen

---
+++ foruma-frontend/src/stories/screens/pure/PureForumScreen.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ContainerSubForumPanel />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ContainerSubForumPanel from "../../components/container/ContainerSubForumPanel.vue";
+
+export default defineComponent({
+  name: "PureForumScreen",
+  components: {
+    ContainerSubForumPanel,
+  },
+});
+</script>
